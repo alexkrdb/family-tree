5fbcc9d4d57c030f35593ea1a0cc2c89bf382b80
alexkrdb <alex.kurdybakha@gmail.com>
2023-08-28 20:47:24 +0200

    Added reactFlow to render trees

--- src/components/tree/Node.jsx
+++ src/components/tree/Node.jsx
@@ -1,15 +1,28 @@
-import {Component} from "react"
+import { Handle, Position } from "reactflow";
+// import { Fragment } from "react";
 
-class Node extends Component{
-    render(){
-        const {person, ...otherProps} = this.props;
-       return (
-        <div className="treeNode" {...otherProps}>
-            <p>{person.firstName}</p>
-            <p>{person.lastName}</p>
-        </div>
-       ) 
-    }
+function Node({ data }) {
+  return (
+    <div className="treeNode">
+      <img src={data.photoUrl} alt="Photo" />
+      <div>
+        <p>{data.firstName + " " + data.lastName}</p>
+        <hr />
+        <p>
+          01.01.1912
+          <br />
+          02.10.2000
+        </p>
+      </div>
+      <div>
+        <button className="addParent">+</button>
+        <button className="addPartner">+</button>
+        <button className="addChild">+</button>
+      </div>
+      <Handle id="parents" type="source" position={Position.Top} />
+      <Handle id="children" type="target" position={Position.Bottom} />
+    </div>
+  );
 }
 
-export default Node
+export default Node;
--- src/pages/Tree/Tree.jsx
+++ src/pages/Tree/Tree.jsx
@@ -1,39 +1,90 @@
-import Node from "../../components/tree/Node" 
+import { useCallback, useMemo, useState } from "react";
+import Node from "../../components/tree/Node";
+import ReactFlow, {
+  Background,
+  Controls,
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+} from "reactflow";
+import "reactflow/dist/style.css";
+
+const persons = [
+  {
+    id: "root",
+    data: {
+      firstName: "John",
+      lastName: "Black",
+      photoUrl: "https://cdn.pixabay.com/photo/2015/08/05/04/25/people-875617_1280.jpg",
+      dateOfBirth: ""
+    },
+    position: { x: 200, y: 400 },
+    type: "treeNode",
+  },
+  {
+    id: "parent_1",
+    data: {
+      firstName: "John",
+      lastName: "Black",
+      photoUrl: "https://cdn.pixabay.com/photo/2015/07/14/06/09/man-844211_1280.jpg",
+    },
+    position: { x: 0, y: 200 },
+    type: "treeNode",
+  },
+  {
+    id: "parent_2",
+    data: {
+      firstName: "John",
+      lastName: "Black",
+      photoUrl: "https://cdn.pixabay.com/photo/2017/06/26/02/47/man-2442565_1280.jpg",
+
+    },
+    position: { x: 400, y: 200 },
+    type: "treeNode",
+  },
+];
+
+const initialEdges = [
+  { id: "root-parent2", source: "root", target: "parent_2", label: "", type: "step" },
+  { id: "root-parent1", source: "root", target: "parent_1", label: "", type: "step" },
+];
+
+const childParentRel = {
+  children: ["root"],
+  parents: ["parent_1", "parent_2"],
+};
+
 function Tree() {
+  const nodeTypes = useMemo(() => ({ treeNode: Node }), []);
 
-  const persons = [
-    {
-      id: "root",
-      firstName: "John",
-      lastName: "Black"
-    },
-    {
-      id: "parent_1",
-      firstName: "John",
-      lastName: "Black"
-    },
-    {
-      id: "parent_2",
-      firstName: "John",
-      lastName: "Black"
-  }];
-  
-  const childParentRel = {
-    children: ["root"],
-    parents: ["parent_1", "parent_2"]
-  };
+  const [nodes, setNodes] = useState(persons);
+  const [edges, setEdges] = useState(initialEdges);
 
-  let x = 0;
-  const getX = () => {
-    x += 100;
-    return {left: x + "px"}
-  } 
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
   return (
     <div className="treePage">
       <div className="pageContent">
-        {persons.map((person)=>(
-          <Node person={person} style={getX()}/>
-        ))}
+        <ReactFlow
+          nodes={nodes}
+          nodeTypes={nodeTypes}
+          onNodesChange={onNodesChange}
+          edges={edges}
+          onEdgesChange={onEdgesChange}
+          fitView
+        >
+          <Background />
+          <Controls />
+        </ReactFlow>
       </div>
     </div>
   );
